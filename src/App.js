31f3a5b462cd4f9c0cfd44364f19a59489bad60e
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2023-09-30 17:51:25 -0400

    context created navigation from dashboard created

--- src/App.js
+++ src/App.js
@@ -7,22 +7,70 @@ import UserDashboard from './routes/userdashboard'
 import AddItem from './routes/additem'
 import OrderStatus from './routes/orderstatus'
 import NotFound from './routes/notfound'
+import React, { useState } from 'react'
 
+export const OrderContext = React.createContext()
+
+const pending = [
+  {
+    orderId: 999,
+    orderStatus: 'pending',
+    intakeMethods: 'delivery',
+    dateRange: {
+      earliest: '2021-01-01',
+      latest: '2021-01-31',
+    },
+    deliveryDate: '2021-01-15',
+    clientInfo: {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: '',
+      phoneNumber: '',
+    },
+    items: [
+      {
+        type: 'sofa',
+        itemStatus: 'aproved',
+        pickable: true,
+        pets: true,
+        smoke: false,
+        waterDamage: false,
+        otherDamage: true,
+        denialReason: '',
+        photos: ['/ashdba', '/asdasd', '/asdasd'],
+      },
+      {
+        type: 'bed',
+        itemStatus: 'aproved',
+        pickable: true,
+        pets: true,
+        smoke: false,
+        waterDamage: false,
+        otherDamage: true,
+        denialReason: '',
+        photos: ['/ashdba', '/asdasd', '/asdasd'],
+      },
+    ],
+  },
+]
 function App() {
+  const [order, setOrder] = useState(pending)
   return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route path='/orderstatus' element={<OrderStatus/>} />
-          <Route path='/additem' element={<AddItem/>} />
-          <Route path='/userdashboard' element={<UserDashboard/>} />
-          <Route path='/admindashboard' element={<Admindashboard/>} />
-          <Route path='/admin' element={<Admin/>} />
-          <Route path='*' element={<NotFound/>} />
-          <Route path='/' element={<Landing/>} />
-        </Routes>
-      </div>
-    </Router>
+    <OrderContext.Provider value={order}>
+      <Router>
+        <div className='App'>
+          <Routes>
+            <Route path='/orderstatus' element={<OrderStatus />} />
+            <Route path='/additem' element={<AddItem />} />
+            <Route path='/userdashboard' element={<UserDashboard />} />
+            <Route path='/admindashboard' element={<Admindashboard />} />
+            <Route path='/admin' element={<Admin />} />
+            <Route path='*' element={<NotFound />} />
+            <Route path='/' element={<Landing />} />
+          </Routes>
+        </div>
+      </Router>
+    </OrderContext.Provider>
   )
 }
 
--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './styles.css'
+import { OrderContext } from '../../App'
+const fallbackImage = './fallback.png'
 
 export default function Landing() {
+  const order = React.useContext(OrderContext)
+  const itemList = order[0].items
+  const navigate = useNavigate()
+  const submitOrder = () => { 
+    alert('submit order')
+    navigate('/orderstatus')
+  }
+
   return (
-    <div id="dashboard-wrapper" class="">
+    <div id='dashboard-wrapper' class='route-wrapper'>
       <h1> Please add your item </h1>
 
-       <div class="dashboard-list-container" >
-        
-          <div >
-            Box
-          </div>
-
-          <div >
-            Box
-          </div>
-
-          <div >
-            Box
-          </div>
-
-       </div>
-
-        <button id="add"> + </button>
-        <button id="submit"> Submit </button>
+      <div class='dashboard-list-container'>
+        {itemList.map(item => {
+          return (
+            <div className='dashboard-item-card'>
+              <div className='dashboard-item-card-image'>
+                {/* <img src={item.photos[0]} onerror={fallbackImage} alt={`image of a ${item.type}`} /> */}
+              </div>
+              <div className='dashboard-item-card-body'>
+                <p> {item.type} </p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className='dashboard-action-menu'>
+        <button className='general-button button-add' onClick={()=> navigate('/additem')}>+</button>
+        <button className='general-button submit' onClick={submitOrder}> Submit </button>
+      </div>
     </div>
   )
 }
