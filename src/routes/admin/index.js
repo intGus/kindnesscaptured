3db8b469558345b25b9886ff7da071dbd955305a
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2023-09-30 14:00:06 -0400

    loging screen created

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -3,6 +3,15 @@ import './styles.css'
 
 export default function Admin() {
   return (
-    <div>Admin</div>
+    <div id='admin-wrapper' className='route-wrapper'>
+      <div id='login-container'>
+        <form id='logging-form'>
+          <h4>Welcome Back</h4>
+          <input type='text' placeholder='Username' />
+          <input type='password' placeholder='Password' />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    </div>
   )
 }
