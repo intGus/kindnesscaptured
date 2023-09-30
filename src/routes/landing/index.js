bc1d4f2cedad359559a7382c7c479ac0461aa005
Alexander Aranda <alexandermarkets1@gmail.com>
2023-09-30 15:36:32 -0400

    landing page, boxes and and buttons, missing functionality

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -3,6 +3,27 @@ import './styles.css'
 
 export default function Landing() {
   return (
-    <div>LANDING</div>
+    <div id="dashboard-wrapper" class="">
+      <h1> Please add your item </h1>
+
+       <div class="dashboard-list-container" >
+        
+          <div >
+            Box
+          </div>
+
+          <div >
+            Box
+          </div>
+
+          <div >
+            Box
+          </div>
+
+       </div>
+
+        <button id="add"> + </button>
+        <button id="submit"> Submit </button>
+    </div>
   )
 }
