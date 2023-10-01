b98143461c981c0e4941b0b377555610493e4ce4
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2023-09-30 21:49:26 -0400

    routes updated

--- src/App.js
+++ src/App.js
@@ -8,14 +8,56 @@ import AddItem from './routes/additem'
 import OrderStatus from './routes/orderstatus'
 import NotFound from './routes/notfound'
 import React, { useState } from 'react'
-import { pending } from './fakevariables'
+// import { pending } from './fakevariables'
 
 export const OrderContext = React.createContext()
 
+const pending = [
+  {
+    orderId: 999,
+    orderStatus: 'pending',
+    intakeMethods: 'delivery',
+    dateRange: {
+      earliest: '2021-01-01',
+      latest: '2021-01-31',
+    },
+    deliveryDate: '2021-01-15',
+    clientInfo: {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: '',
+      phoneNumber: '',
+    },
+    items: [
+      {
+        type: 'sofa',
+        itemStatus: 'aproved',
+        pickable: true,
+        pets: true,
+        smoke: false,
+        waterDamage: false,
+        otherDamage: true,
+        denialReason: '',
+        photos: ['/ashdba', '/asdasd', '/asdasd'],
+      },
+      {
+        type: 'bed',
+        itemStatus: 'aproved',
+        pickable: true,
+        pets: true,
+        smoke: false,
+        waterDamage: false,
+        otherDamage: true,
+        denialReason: '',
+        photos: ['/ashdba', '/asdasd', '/asdasd'],
+      },
+    ],
+  },
+]
 function App() {
   const [order, setOrder] = useState(pending)
   return (
-    <OrderContext.Provider value={order}>
+    <OrderContext.Provider value={pending}>
       <Router>
         <div className='App'>
           <div className='App-header'>
@@ -25,12 +67,13 @@ function App() {
           </div>
           <Routes>
             <Route path='/orderstatus' element={<OrderStatus />} />
-            <Route path='/additem' element={<AddItem />} />
             <Route path='/userdashboard' element={<UserDashboard />} />
             <Route path='/admindashboard' element={<Admindashboard />} />
             <Route path='/admin' element={<Admin />} />
             <Route path='*' element={<NotFound />} />
-            <Route path='/' element={<Landing />} />
+            {/* <Route path='/additem' element={<AddItem />} /> */}
+            {/* <Route path='/' element={<Landing />} /> */}
+            <Route path='/' element={<AddItem />} />
           </Routes>
         </div>
       </Router>
